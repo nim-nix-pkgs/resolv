2db3f86870808d5e35fbb68e9dc63b90a437582d
hugosenari <hugosenari@gmail.com>
2022-03-12 02:38:22 -0300

    chore: re index resolv

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''DNS resolution nimble making use of the native glibc resolv library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."resolv-master".url = "path:./master";
+  inputs."resolv-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."resolv-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''DNS resolution nimble making use of the native glibc resolv library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-resolv-master.flake = false;
+  inputs.src-resolv-master.owner = "mildred";
+  inputs.src-resolv-master.ref   = "refs/heads/master";
+  inputs.src-resolv-master.repo  = "resolv.nim";
+  inputs.src-resolv-master.type  = "github";
+  
+  inputs."dnsprotocol".dir   = "nimpkgs/d/dnsprotocol";
+  inputs."dnsprotocol".owner = "riinr";
+  inputs."dnsprotocol".ref   = "flake-pinning";
+  inputs."dnsprotocol".repo  = "flake-nimble";
+  inputs."dnsprotocol".type  = "github";
+  inputs."dnsprotocol".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."dnsprotocol".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-resolv-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-resolv-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
